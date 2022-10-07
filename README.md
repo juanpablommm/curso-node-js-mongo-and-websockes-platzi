d47b08cd83fedea75de28c4a22a00e1c5465b75c
Juan Pablo Montoya Ospinal <62717509+juanpablommm@users.noreply.github.com>
2022-10-06 22:19:32 -0500

    :pencil2: clase-2 analizando el query y body en la peticion, mediante los middlewares de json, y urlencoded

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 ### CURSO DE NODE.JS: BASE DE DATOS MONGODB Y WEBSOCKETS PLATZI
 
-clase 1. instalacion de [express](https://www.npmjs.com/package/express) para trabajar con servidores.
-este es el paquete mas utilizado para trabajar con srvidores de una manera mas sencilla y rapida, en comparacion con la creacion a apartir del modulo http;
-se crea un constrollador basico, para escuchar bajo los verbos, get, post, update, delete, respondiendo con un simple mensaje de igual manera creamos un middleware para usarlo antes de cada peticion.
-
-**middleware =>** un middleware es una funcion o pieza de codigo que se ejecutara 
-despues de que el servidor resiba una llamada desde un cliente y antes de 
-la accion del controlador, independitemente de cual sea el verbo 
-poir el que estamos escuchando. lo que significa que antes cuando se realiza un
-reques, se ejecutara el middleware antes de que el controllador mande el response, este se susle utlizar con diferentes propositos como resgistar las solicitudes o verificarlas para algun requisito especifico, como puede ser verificar si viene una cabecera en esta
+clase 2, trabajando con el body y el query de la peticion. anteriormente para trabajar con el 
+body de la peticion en express era necesario instalar un modulo extra llamado **body-parser** y del cual teneia mos que realizar la importacion y posteriormente pasarlo como **middleware** a express en el metodo use. Ahora esto ya no es necesario dado que express ya trae traen los middlewares necesarios para las peticiones.
+utlizamos los midllewares **express.json()** para lectura del json que venga en el cuerpo de la
+peticion, y utlizamos **express.urlencoded({extended: true})** para lectura del body de la peticion en caso que el content-tyep sea tipo unlencoded
--- server.mjs
+++ server.mjs
@@ -1,27 +1,48 @@
 import express from 'express';
 import miMiddleware from './middleware.mjs';
+//const bodyParse = require('body-parse');
+/**anteriormente para trabajar con el body de la peticion debiamos inslar la dependencia
+ * de body-parse importarla y psarla como middlewara a express, ahora esto ya no es necesario,
+ * dado que express ya trae los middlewares necesarios para trabajar con las peticions y estas.
+ */
 
-/**inicializamos express */
 const app = express();
 
-/** l metodo use, de express lo usaremos para pasar un middleware que se ejcutara antes de que 
- * nuestro contoroladores responda al cliente, con alguno de los end-points definidos
+//app.use(bodyPase); no es necesario
+
+app.use(express.json());
+/**ahora para trabajar con el body de la peticion podemos apoyarnos de express.json, este seria
+ * el middleware para trabajar con el body, cabe recalcar que los midleware al ser funciones o
+ * piezas de codigo que se ejcutaran antes de que el coontrolador resonda, debemo saber que 
+ * middleware jecutamos primero, antes que el otro, por ejemplo si vamos a validar sobre
+ * el body algo, primero entonces este middleware es el que deberia ser el que le pasemos
+ * primero a express
  */
+
+app.use(express.urlencoded({extended: true})) //este middleware lo que ara es poder examinar el
+//cuyo body sea de tipo unlencoded cuya cabecera de la peticion sea Content-Type: application/x-www-form-urlencoded
 app.use(miMiddleware);
 
-//method por get
 app.get('/', (request, response) => {
+    /**obentemos el body de la peticion, pdodiendo leerlo si es un json, o urlencoded, 
+     * de acorte de la cabecera Content-Type y la forma que enviemos
+     */
+    console.log(request.body);
+    
     response.send('hello word from get method');
 })
 
 app.post('/message', (request, response) => {
-    response.send('hemos añadido un mensaje');
+    /**ahora si queremos obtner los paramtros que viene por url en la peticion,
+     * simplemente basta con que los obtengamos del request
+     */
+    console.log('parametros por query' + JSON.stringify(request.query));
+    response.send('hemos añadido un mensaje para ' + JSON.stringify(request.query));
 })
 
 app.delete('/message', (request, response) => {
     response.send('hemos eliminado un mensaje');
 });
 
-/**mencionamos a express por que puerto va a escuchar la aplicacion */
 app.listen(3000);
 console.log('la aplicacion esta escuchando en http://localhost:3000');
