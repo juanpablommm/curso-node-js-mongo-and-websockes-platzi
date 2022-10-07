17cc7525c7f7044b665639b708232be3fdd19b70
Juan Pablo Montoya Ospinal <62717509+juanpablommm@users.noreply.github.com>
2022-10-06 23:00:46 -0500

    :pencil2: clase 3, analizando los header en el reuqest y response de la peticion

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 ### CURSO DE NODE.JS: BASE DE DATOS MONGODB Y WEBSOCKETS PLATZI
 
-clase 2, trabajando con el body y el query de la peticion. anteriormente para trabajar con el 
-body de la peticion en express era necesario instalar un modulo extra llamado **body-parser** y del cual teneia mos que realizar la importacion y posteriormente pasarlo como **middleware** a express en el metodo use. Ahora esto ya no es necesario dado que express ya trae traen los middlewares necesarios para las peticiones.
-utlizamos los midllewares **express.json()** para lectura del json que venga en el cuerpo de la
-peticion, y utlizamos **express.urlencoded({extended: true})** para lectura del body de la peticion en caso que el content-tyep sea tipo unlencoded
+clase 3, manejando los **headers**, para leer las cabeceras del request, de la peticcion que hagamos,simplemnte en nustro metodo get, post, o demas verbo que estemos utlizando, solamente
+tendremos que llamar a los headers ```request.header``` y obtendremos los headers que veine en la peticion: ```js 
+{
+  'content-type': 'application/x-www-form-urlencoded',
+  task: 'nose',
+  'user-agent': 'PostmanRuntime/7.29.2',
+  accept: '*/*',
+  'postman-token': '62f43cb1-e18a-4aaf-b3f6-6e27c015fd1b',
+  host: 'localhost:3000',
+  'accept-encoding': 'gzip, deflate, br',
+  connection: 'keep-alive',
+  'content-length': '9'
+} ```
+
+entre las cabeceras mas comunes que podemos encontrar en una peticion: 
+* **user-agent** que nos mensionara el agente desde donde realizamos la peticion, por ejemplo postman, chrome, brave, etc.
+* **host** que nos dira el host de la peticion
+* **content-type** el cual diara el tipo de contenido de la peticion
+* **cahce-control** para setear cache especifico para imagenes, archivos de js, ect
+
+**mandar headers en el response**
+para mandar headers en el response simplmente ```response.header({
+    "custom-header": "Mi valor personalizado"
+})```
+donde simplmente pasmo un objecto json como paramtro a headers en el response, y vamos colocando nuestra cabeceras que necesitemos enviar con su respectivo valor
--- server.mjs
+++ server.mjs
@@ -1,41 +1,28 @@
 import express from 'express';
 import miMiddleware from './middleware.mjs';
-//const bodyParse = require('body-parse');
-/**anteriormente para trabajar con el body de la peticion debiamos inslar la dependencia
- * de body-parse importarla y psarla como middlewara a express, ahora esto ya no es necesario,
- * dado que express ya trae los middlewares necesarios para trabajar con las peticions y estas.
- */
+
 
 const app = express();
 
-//app.use(bodyPase); no es necesario
 
 app.use(express.json());
-/**ahora para trabajar con el body de la peticion podemos apoyarnos de express.json, este seria
- * el middleware para trabajar con el body, cabe recalcar que los midleware al ser funciones o
- * piezas de codigo que se ejcutaran antes de que el coontrolador resonda, debemo saber que 
- * middleware jecutamos primero, antes que el otro, por ejemplo si vamos a validar sobre
- * el body algo, primero entonces este middleware es el que deberia ser el que le pasemos
- * primero a express
- */
 
-app.use(express.urlencoded({extended: true})) //este middleware lo que ara es poder examinar el
-//cuyo body sea de tipo unlencoded cuya cabecera de la peticion sea Content-Type: application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true})) 
+
 app.use(miMiddleware);
 
 app.get('/', (request, response) => {
-    /**obentemos el body de la peticion, pdodiendo leerlo si es un json, o urlencoded, 
-     * de acorte de la cabecera Content-Type y la forma que enviemos
-     */
+    //obetenemos lo header del request
+    console.log(request.headers);
     console.log(request.body);
-    
+    //enviamos header en el response
+    response.header({
+        "custom-header": "mi valor personalizado",
+    });
     response.send('hello word from get method');
 })
 
 app.post('/message', (request, response) => {
-    /**ahora si queremos obtner los paramtros que viene por url en la peticion,
-     * simplemente basta con que los obtengamos del request
-     */
     console.log('parametros por query' + JSON.stringify(request.query));
     response.send('hemos añadido un mensaje para ' + JSON.stringify(request.query));
 })
